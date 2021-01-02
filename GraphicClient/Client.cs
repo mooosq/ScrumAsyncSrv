70e7f5b8d110173af5cb3337d9841ff70ea5ebda
Tobiasz Komarnicki <tobiasz.komarnicki@gmail.com>
2021-01-02 18:47:13 +0100

    Task #10 - first version of graphical client

---
+++ GraphicClient/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicClient
+{
+    class Client
+    {
+        Socket socket;
+
+        public Client(string ipAddr, string port)
+        {
+            IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse(ipAddr), int.Parse(port));
+            socket = new Socket(remoteEP.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(remoteEP);
+        }
+
+        public string Receive()
+        {
+            byte[] bytes = new byte[1024];
+
+            int bytesRec = socket.Receive(bytes);
+            return Encoding.ASCII.GetString(bytes, 0, bytesRec);
+        }
+    }
+}
---
+++ GraphicClient/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicClient
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Client client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void connectToServer_click(object sender, RoutedEventArgs e)
+        {
+            if (ipAddr.Text != "" && port.Text != "")
+            {
+                try
+                {
+                    client = new Client(ipAddr.Text, port.Text);
+                    ipAddr.Visibility = Visibility.Hidden;
+                    port.Visibility = Visibility.Hidden;
+                    Connect.Visibility = Visibility.Hidden;
+                    ipAddrLabel.Visibility = Visibility.Hidden;
+                    portLabel.Visibility = Visibility.Hidden;
+                    clientLog.Visibility = Visibility.Visible;
+                    clientCommand.Visibility = Visibility.Visible;
+                    sendCommand.Visibility = Visibility.Visible;
+                    this.Height = 350;
+                    this.Width = 800;
+
+                    clientLog.Text += client.Receive();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+            else
+                MessageBox.Show("Fields can not be empty!", "Error");
+        }
+    }
+}
